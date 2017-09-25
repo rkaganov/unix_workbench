0e10ef58dd1d8ae117526e6d6f64a4e1023fa75a
U-MARVELL\roman <roman@YK-LT0084.marvell.com>
2017-09-25 17:28:51 +0300

    initial commit

---
+++ guessinggame.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+function xfiles {
+amount=$(find . -maxdepth 1 -type f| wc -l)
+echo "function amount=$amount"
+}
+
+xfiles
+
+while true
+do
+	echo "Enter number of files in current directory , followed by [ENTER]:"
+	read fc
+	echo "fc=$fc; amount=$amount"
+	if [[ $fc -eq $amount ]] 
+	then
+		echo "Right Guess!!"
+		break
+	fi
+done
+
+
